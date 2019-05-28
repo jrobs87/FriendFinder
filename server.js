aa51061666eb1f10d7f367adf01c6c647c1dfcff
John Robertson <tiktaalikdesign@gmail.com>
2019-05-27 22:18:03 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// bringing in express and setting up server
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path = require('path');
+
+// needed to serve the css in public
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/home.html')))
+
+// why is public not needed?  because of the realtive filepath?
+app.get('/survey', (req, res) => res.sendFile(path.join(__dirname + '/public/survey.html')))
+
+app.listen(port, () => console.log(`Friend Finder App listening on port ${port}!`))
