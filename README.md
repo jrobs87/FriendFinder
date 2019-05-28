d18c314acb504bcb8544532245ddc0aff9b7da41
John Robertson <tiktaalikdesign@gmail.com>
2019-05-28 11:40:35 -0400

    post route to api/friends working

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project Title
+# FriendFinder
 
 One Paragraph of project description goes here
 
--- server.js
+++ server.js
@@ -14,4 +14,6 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/home.html
 // why is public not needed?  because of the realtive filepath?
 app.get('/survey', (req, res) => res.sendFile(path.join(__dirname + '/public/survey.html')))
 
+app.post('/api/friends', (req, res) => res.send('hi')); // works, now to append the array in the file
+
 app.listen(port, () => console.log(`Friend Finder App listening on port ${port}!`))
