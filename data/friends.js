118415ef390e924202a474e9f4a39118334920a5
John Robertson <tiktaalikdesign@gmail.com>
2019-05-28 21:29:39 -0400

    functionality all set up.  need to seed profiles, add json res data to modal, rename some var and tidy up the code, add comments

---
+++ data/friends.js
@@ -0,0 +1,36 @@
+let profiles = [
+    {
+        name: 'Sweet Dee',
+        photo: '',
+        answers: [
+            1,
+            1,
+            1,
+            2,
+            2,
+            1,
+            3,
+            4,
+            3,
+            3
+        ]
+    },
+    {
+        name: 'Frank Reynolds',
+        photo: '',
+        answers: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            4,
+            3,
+            5
+        ]
+    }
+];
+
+exports.profiles = profiles;
--- server.js
+++ server.js
@@ -4,16 +4,58 @@ const app = express()
 const port = 3000
 
 const path = require('path');
+const fs = require('fs');
+
+const friends = require('./data/friends.js')
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ===============================================================================================
 
 // needed to serve the css in public
 app.use(express.static(__dirname + '/public'));
 
+// ===============================================================================================
 
+// HTML routes 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/home.html')))
 
 // why is public not needed?  because of the realtive filepath?
 app.get('/survey', (req, res) => res.sendFile(path.join(__dirname + '/public/survey.html')))
 
-app.post('/api/friends', (req, res) => res.send('hi')); // works, now to append the array in the file
+// ===============================================================================================
+
+// API routes
+app.post('/api/friends', (req, res) => {
+    // console.log(req.body);
+    let user = req.body;
+    
+    // friends.profiles.push(req.body);
+    console.log(friends.profiles);
+    
+    let currentDiff = 10;
+    let currentMatch = '';
+
+    for (i = 0; i < friends.profiles.length; i ++) {
+        diff = 0;
+        let profile = friends.profiles[i];
+        for (z = 0; z < 10; z ++) {
+            test = Math.abs(user.answers[z] - profile.answers[z]);
+            diff =+ test;
+        }
+      console.log(diff);
+      if (diff < currentDiff) {
+          console.log('New Match!');
+          currentDiff = diff;
+          currentMatch = profile;
+      }
+      console.log(`Your best match is ${currentMatch.name}`);
+    }
+
+    res.send(currentMatch)}); // works, now to append the array in the file
+
+// ===============================================================================================
 
 app.listen(port, () => console.log(`Friend Finder App listening on port ${port}!`))
