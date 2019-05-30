6dbf70e0f26ba5d49e5af513d21bf47ca30ee683
John Robertson <tiktaalikdesign@gmail.com>
2019-05-29 22:50:50 -0400

    fixed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // bringing in express and setting up server
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 const path = require('path'); // dont need 
 
