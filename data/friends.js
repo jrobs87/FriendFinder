026dc53ed7c15d4a2aa7b00ac7e6625699284710
John Robertson <tiktaalikdesign@gmail.com>
2019-05-30 10:29:10 -0400

    added more profiles + css tweaks

--- data/friends.js
+++ data/friends.js
@@ -11,23 +11,23 @@ let profiles = [
             1,
             3,
             4,
-            3,
-            3
+            1,
+            1
         ]
     },
     {
         name: 'Andrew',
         photo: 'andrew.jpg',
         answers: [
+            5,
+            5,
+            5,
+            1,
+            5,
             1,
             1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
+            5,
+            5,
             1
         ]
     },
@@ -45,6 +45,70 @@ let profiles = [
             5,
             5,
             5
+        ]
+    },
+    {
+        name: 'Sam',
+        photo: 'sam.jpg',
+        answers: [
+            2,
+            2,
+            2,
+            3,
+            2,
+            4,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        name: 'Addie',
+        photo: 'Addie.jpg',
+        answers: [
+            1,
+            2,
+            1,
+            1,
+            2,
+            3,
+            1,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        name: 'Andy',
+        photo: 'andy.jpg',
+        answers: [
+            5,
+            3,
+            5,
+            3,
+            5,
+            3,
+            5,
+            3,
+            5,
+            3
+        ]
+    },
+    {
+        name: 'Phillip',
+        photo: 'Phillip.jpg',
+        answers: [
+            3,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            2,
+            2
         ]
     }
 ];
